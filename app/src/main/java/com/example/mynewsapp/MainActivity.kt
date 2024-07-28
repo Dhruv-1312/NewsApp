7bf2f267dbc6d6a7756c343fede06e724dec7ca7
Dhruv Dhall <dhruvdhall86@gmail.com>
2024-07-29 02:35:59 +0530

    feat:onboarding page

--- app/src/main/java/com/example/mynewsapp/MainActivity.kt
+++ app/src/main/java/com/example/mynewsapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.mynewsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mynewsapp.presentation.onboarding.OnBoardingScreen
 import com.example.mynewsapp.ui.theme.MyNewsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyNewsAppTheme {
                 // A surface container using the 'background' color from the theme
-
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background))
+                OnBoardingScreen()
             }
         }
     }
