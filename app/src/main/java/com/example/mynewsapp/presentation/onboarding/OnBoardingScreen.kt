e511e3d0c3e062902cb9da8f7afd572528aa0722
Dhruv Dhall <dhruvdhall86@gmail.com>
2024-08-08 23:47:38 +0530

    fix:onboarding screen fixes

--- app/src/main/java/com/example/mynewsapp/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/example/mynewsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -1,15 +1,32 @@
 package com.example.mynewsapp.presentation.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.mynewsapp.presentation.onboarding.common.NewsButton
 import com.example.mynewsapp.presentation.onboarding.components.OnBoardingPage
+import com.example.mynewsapp.presentation.onboarding.components.PagerIndicator
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -23,6 +40,7 @@ fun OnBoardingScreen() {
                 when (pagerState.currentPage) {
                     0 -> listOf("", "Next")
                     1 -> listOf("Back", "Next")
+                    2 -> listOf("Back", "Get Started")
                     else -> listOf("", "")
                 }
             }
@@ -30,6 +48,40 @@ fun OnBoardingScreen() {
         HorizontalPager(state = pagerState) { index ->
             OnBoardingPage(page = pages[index])
         }
+
+        
+        Spacer(modifier = Modifier.weight(1f))
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp)
+            .navigationBarsPadding(), Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically,) {
+            PagerIndicator(modifier = Modifier.width(52.dp),pagesSize = pages.size, selectedPage =pagerState.currentPage ,)
+            Row(verticalAlignment =Alignment.CenterVertically){
+                val scope= rememberCoroutineScope()
+                if(buttonState.value[0].isNotEmpty()){
+                    NewsButton(text = buttonState.value[0], onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage( page=pagerState.currentPage-1)
+                        }
+                    })
+
+                }
+                NewsButton(text = buttonState.value[1], onClick = {
+                    scope.launch {
+                        if(pagerState.currentPage== 3){
+                            //TODO
+                        }else{
+                            pagerState.animateScrollToPage( page=pagerState.currentPage+1)
+                        }
+                    }
+                })
+            }
+        }
+
+
+
+        
 
     }
 
